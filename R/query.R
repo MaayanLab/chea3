14a77f824dfcbfb6728b8af662f2d057f8bcefaa
Alexandra Keenan <alexandra.b.keenan@gmail.com>
2018-07-16 14:07:46 -0400

    Added wrapper function for chea query for web functionality

--- R/query.R
+++ R/query.R
@@ -4,7 +4,8 @@
 #'
 #' @param set_name Name of query. Defaults to "untitled".
 #' @param geneset Character array of gene set to query
-#' @param n_results Number of results to return for each TF regulon library. Defaults to 10.
+#' @param n_results Number of results to return for each TF regulon library. Defaults to 10. Input "all"
+#' to return results for all transcription factors.
 #' @param libnames Character array of TF regulon library names to use. Defaults to all libraries available in the package.
 #' @param integrate_libs Integrate library results. Often results in a better transcription factor ranking. Defaults to TRUE.
 #' @param method Method(s) for comparison of TF regulons and query. Defaults to Fisher's Exact Test
@@ -15,7 +16,7 @@
 #' results = queryChea3(set_name = "exp1_DEG",example_query)
 #'
 
-queryChea3 = function(
+queryChea = function(
   geneset,
   set_name = "untitled",
   n_results = 10,
@@ -39,6 +40,7 @@ queryChea3 = function(
   #list of results data frames
 
   results = lapply(my_libs,function(x){
+    x = genesetr::removeLibWeights(x)
     df = genesetr::pairwiseSetOverlap(query, x,
       background = background, method = method)
     df$rank = rank(df$FET.p.val,ties.method = "random")/nrow(df)
@@ -56,17 +58,59 @@ queryChea3 = function(
       sub = sub[order(sub$rank),][1,]
       return(sub)
     })
+    int_results$prev_rank = int_results$rank
     int_results$rank = rank(int_results$rank,ties.method = "random")
 
     results[["integrated"]] = int_results
 
     results = lapply(results,function(sub){
       sub$rank = rank(sub$rank,ties.method = "random")
+      if(n_results == "all"){
+        return(sub[order(sub$rank),])
+      }
       return(sub[order(sub$rank),][1:n_results,])
     })
   }
 
-  return(jsonlite::toJSON(results))
+  return(results)
 }
 
+
+
+#' Web query ChEA3
+#'
+#' Serves as a wrapper function for queryChea()
+#'
+#' @param set_name Name of query. Defaults to "untitled".
+#' @param geneset Character array of gene set to query
+#' @param n_results Number of results to return for each TF regulon library. Defaults to 10. Input "all"
+#' to return results for all transcription factors.
+#' @param libnames Character array of TF regulon library names to use. Defaults to all libraries available in the package.
+#' @param integrate_libs Integrate library results. Often results in a better transcription factor ranking. Defaults to TRUE.
+#' @param method Method(s) for comparison of TF regulons and query. Defaults to Fisher's Exact Test
+#' @param background The size of the universe of genes under consideration. User may set to a constant. Default is NULL which results in background being set to the size of the union of the unique genes of the query and TF regulon library.
+#' @return A json of datasets with the top TF regulon hits for each TF regulon library tested.
+#' @examples
+#' data(example_query)
+#' results = queryChea3(set_name = "exp1_DEG",example_query)
+#'
+queryCheaWeb = function(
+  geneset,
+  set_name = "untitled",
+  n_results = 10,
+  libnames = names(chea3::libs),
+  integrate_libs = T,
+  method = c("FET"),
+  background = NULL){
+
+  results = queryChea(geneset = geneset,
+    set_name = set_name,
+    n_results = n_results,
+    libnames = libnames,
+    integrate_libs = integrate_libs,
+    method = method,
+    background = background)
+
+  return(jsonlite::toJSON(results))
+}
 
