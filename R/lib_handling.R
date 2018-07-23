66fb1397e07e2670af10fd2c9f6db48e9d5149d9
Alexandra Keenan <alexandra.b.keenan@gmail.com>
2018-07-23 10:49:26 -0400

    Added function documentation.

--- R/lib_handling.R
+++ R/lib_handling.R
@@ -2,6 +2,14 @@ getLibNames = function(){
   return(names(libs))
 }
 
+#' Returns lib in form of a list.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
 getLib = function(name){
   return(libs[[name]])
 }
