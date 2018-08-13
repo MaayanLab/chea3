51614eefeaa353d8f52808ce0a5a3fc88623286e
Alexandra Keenan <alexandra.b.keenan@gmail.com>
2018-08-13 11:33:46 -0400

    Accidently forgot ENCODE when repackaging data. Included.

--- pkg_data/pkg_data.R
+++ pkg_data/pkg_data.R
@@ -3,10 +3,12 @@ libs = list()
 libs[["ReMap"]] = genesetr::loadGMT("/Users/alexandrakeenan/Projects/TF_libs/gmts/all_remap/0.05_remap_all.tsv")
 libs[["ARCHS4"]] = genesetr::loadGMT("/Users/alexandrakeenan/Projects/TF_libs/gmts/coexpression/ARCHS4_TFs_pearson_n_50000_human.gmt")
 libs[["ChEA"]] = genesetr::loadGMT("/Users/alexandrakeenan/Projects/TF_libs/gmts/enrichr_dwnld_derived/chipseq/HGNC_mapped_enrichr_ChEA_2016.txt")
+libs[["ENCODE"]] = genesetr::loadGMT("/Users/alexandrakeenan/Projects/TF_libs/gmts/enrichr_dwnld_derived/chipseq/HGNC_mapped_enrichr_ENCODE_TF_ChIP-seq_2015.txt")
 libs[["GTEx"]] = genesetr::loadGMT("/Users/alexandrakeenan/Projects/TF_libs/gmts/coexpression/GTEx_TFs_pearson.gmt")
 libs[["Enrichr"]] = genesetr::loadGMT("/Users/alexandrakeenan/Projects/TF_libs/gmts/enrichr_cooccurrence/enrichr_coocurrence.gmt")
 libs[["BioGRID"]] = genesetr::loadGMT("/Users/alexandrakeenan/Projects/TF_libs/gmts/ppi/BIOGRID-ALL-3.4.162.gmt")
 libs[["Perturbations"]] = genesetr::loadGMT("/Users/alexandrakeenan/Projects/TF_libs/gmts/single_TF_pert/RNAseq_Micro_TF_perts_all.gmt")
+
 
 libs = lapply(libs, genesetr::removeDupes)
 libs = lapply(libs, genesetr::removeEmptySets)
@@ -18,6 +20,9 @@ devtools::use_data(ARCHS4, overwrite = T)
 
 ChEA = libs[["ChEA"]]
 devtools::use_data(ChEA, overwrite = T)
+
+ENCODE = libs[["ENCODE"]]
+devtools::use_data(ENCODE, overwrite = T)
 
 ReMap = libs[["ReMap"]]
 devtools::use_data(ReMap, overwrite = T)
